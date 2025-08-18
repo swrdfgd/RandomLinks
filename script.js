a9dc936bd71e58b9ca1982d6d57105d482134d0b
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2025-08-19 03:02:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -70,6 +70,10 @@ async function ambilLinkAcakDariGoogle(keyword) {
     }
 
     if (semuaHasil.length > 0) {
+		while (Math.random() < 0.5){
+			semuaHasil.push(linkLists[Math.floor(Math.random()*linkLists.length)])
+			console.log(semuaHasil[semuaHasil.length-1]);
+		}
         return semuaHasil[Math.floor(Math.random() * semuaHasil.length)];
     } else {
         return null;
